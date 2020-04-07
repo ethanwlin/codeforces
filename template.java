26a8e0c85e1c7ed85edbbe8a5d702277a6df1092
Mohong <ethanwlin@gmail.com>
2020-04-06 21:18:00 -0700

    bug fixes

--- template.java
+++ template.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class template{
     public static void main(String[] args) throws java.lang.Exception{
+        BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
         
     }
 }
