f2792682d232400fbb2188a7e23b3fc91e543bc4
Mohong <ethanwlin@gmail.com>
2020-04-06 21:46:18 -0700

    template

--- template.java
+++ template.java
@@ -4,6 +4,6 @@ import java.io.*;
 public class template{
     public static void main(String[] args) throws java.lang.Exception{
         BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
-        
+
     }
 }
