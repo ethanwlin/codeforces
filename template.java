a3246607f6b0cde858c76ecd7fbe530d2481440a
Mohong <ethanwlin@gmail.com>
2020-04-15 13:30:09 -0700

    Rewrote in c++

--- template.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-import java.io.*;
-
-public class template{
-    public static void main(String[] args) throws java.lang.Exception{
-        BufferedReader reader =  new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(reader.readLine());
-    }
-}
---
+++ watermelon.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    int weight;
+    cin >> weight;
+
+    if( weight % 2 == 0 && weight > 2) cout << "YES" << "\n";
+    else cout << "NO" << "\n";   
+}
